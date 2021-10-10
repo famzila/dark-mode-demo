b2908dcc4693db6bf724389b8e15edefd8f7809d
famzila <famzil.contact@gmail.com>
2021-10-10 15:33:13 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'reactstrap';
+import { ThemeContext, themes } from './contexts/ThemeContext';
+import React from 'react';
+import ToggleDark from './components/toggleDark';
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-warning">Dark/Light mode</h1>
       </header>
+      <ThemeContext.Consumer>
+        {({ changeTheme }) => (
+          <ToggleDark
+            toggleDark={() => {
+              setDarkMode(!darkMode);
+              changeTheme(darkMode ? themes.light : themes.dark);
+            }}
+          />
+        )}
+      </ThemeContext.Consumer>
+
+      <footer className="center footer">
+        <Container>
+          <a
+            href="https://dribbble.com/khatib"
+            target="_blank"
+            rel="noreferrer"
+            color="gray"
+          >
+            Design Inspiration from Mo(@Khatib)
+          </a>
+        </Container>
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ThemeContextWrapper from './theme/ThemeWrapper';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeContextWrapper>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>{' '}
+  </ThemeContextWrapper>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
